59349f50f320bf6c7f02fb564b4ad7ec7184e24d
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-04 22:02:39 -0400

    Add files via upload

---
+++ intro_to_hash.rb
@@ -0,0 +1,22 @@
+#hash is a data structure consisting of key value pairs 
+#key is an identifier for a value 
+#hash solves problem of association (connecting two values together)
+
+#example: restaurant menu connects a food item to a price 
+#example: a dictionary connects a word to a definition 
+
+#rules of a hash 
+#hash keys must be unique 
+#hash values can contain duplicates 
+#hash values are extracted by key, not by order 
+
+empty_hash = {}
+puts empty_hash
+puts empty_hash.class 
+
+empty_array = []
+p empty_array 
+puts empty_array.class 
+
+puts empty_hash.length 
+puts empty_hash.empty? 
