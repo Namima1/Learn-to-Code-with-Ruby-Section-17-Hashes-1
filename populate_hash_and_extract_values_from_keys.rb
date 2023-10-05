91185da833db87324119c967a695683835616722
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-04 22:26:27 -0400

    Add files via upload

---
+++ populate_hash_and_extract_values_from_keys.rb
@@ -0,0 +1,13 @@
+#hash key-value assignment syntax 
+#key => value 
+#hash rocket 
+
+nfl_roster_salaries = {
+  "Patrick Mahomes" => 50_000_000, 
+  "Josh Allen" => 25_000_000, 
+  "Joe Burrow" => 12_000_000
+}
+
+p nfl_roster_salaries.length 
+
+#total length is the number of key value pairs. 
