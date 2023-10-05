8fac47664065f3e760eb31ffb7117542c498b25e
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-05 11:51:19 -0400

    Add files via upload

---
+++ symbols_as_hash_keys.rb
@@ -0,0 +1,12 @@
+person = {
+  :name => "Naima", 
+  :age => 26, 
+  :Beautiful => true     
+}
+#hash has curly braces {}
+#hash can be a collection of information, name or age. 
+#Value is what is the value of that identifier/key 
+
+puts person[:name]
+puts person[:age]
+puts person[:Beautiful]
